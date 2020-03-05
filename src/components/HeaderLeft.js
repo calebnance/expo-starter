f128536734864e58e6c5c0e468c7c73959f9a8e8
Caleb Nance <calebnance@gmail.com>
2020-03-05 00:14:14 -0500

    Component: Nav Header Left & Right

---
+++ src/components/HeaderLeft.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { gStyle } from '../constants';
+
+const HeaderLeft = ({ icon, onPress, text }) => (
+  <TouchableOpacity
+    activeOpacity={gStyle.activeOpacity}
+    onPress={onPress}
+    style={[gStyle.flexRowAlignCenter, gStyle.pL2]}
+  >
+    {icon && <View style={gStyle.mR1}>{icon}</View>}
+    {text && <Text>{text}</Text>}
+  </TouchableOpacity>
+);
+
+HeaderLeft.defaultProps = {
+  icon: null,
+  onPress: () => null,
+  text: null
+};
+
+HeaderLeft.propTypes = {
+  // optional
+  icon: PropTypes.element,
+  onPress: PropTypes.func,
+  text: PropTypes.string
+};
+
+export default HeaderLeft;
---
+++ src/components/HeaderRight.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { gStyle } from '../constants';
+
+const HeaderRight = ({ icon, onPress, text }) => (
+  <TouchableOpacity
+    activeOpacity={gStyle.activeOpacity}
+    onPress={onPress}
+    style={[gStyle.flexRowAlignCenter, gStyle.pR2]}
+  >
+    {text && <Text>{text}</Text>}
+    {icon && <View style={gStyle.mL1}>{icon}</View>}
+  </TouchableOpacity>
+);
+
+HeaderRight.defaultProps = {
+  icon: null,
+  onPress: () => null,
+  text: null
+};
+
+HeaderRight.propTypes = {
+  // optional
+  icon: PropTypes.element,
+  onPress: PropTypes.func,
+  text: PropTypes.string
+};
+
+export default HeaderRight;
