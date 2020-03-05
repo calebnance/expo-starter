542e594af4d2bcbcd919c71482db648442e1994c
Caleb Nance <calebnance@gmail.com>
2020-03-04 22:45:44 -0500

    added StatusBar to app root

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 // root stack navigation
 import RootStack from './src/navigation/RootStack';
 
 export default class App extends React.Component {
   render() {
-    return <RootStack />;
+    return (
+      <React.Fragment>
+        <StatusBar barStyle="dark-content" />
+        <RootStack />
+      </React.Fragment>
+    );
   }
 }
