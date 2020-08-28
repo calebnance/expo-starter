e617df28578e40f8b658a8daeb2d0e627b3d37db
Caleb Nance <calebnance@gmail.com>
2020-08-27 22:14:26 -0400

    revert back to airbnb, tried something, didn't like it

--- App.js
+++ App.js
@@ -4,13 +4,11 @@ import { StatusBar } from 'react-native';
 // root stack navigation
 import RootStack from './src/navigation/RootStack';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <StatusBar barStyle="dark-content" />
-        <RootStack />
-      </React.Fragment>
-    );
-  }
-}
+const App = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <RootStack />
+  </>
+);
+
+export default App;
