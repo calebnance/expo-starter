d9995a24edef2fcfa6da14f345f66108dfa49213
Caleb Nance <calebnance@gmail.com>
2020-10-03 22:51:36 -0400

    fixed the import of React

--- src/icons/Svg.Menu.js
+++ src/icons/Svg.Menu.js
@@ -1,5 +1,5 @@
+import * as React from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 import Svg, { Path } from 'react-native-svg';
 import { colors } from '../constants';
 
