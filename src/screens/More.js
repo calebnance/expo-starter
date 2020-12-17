0ba3439967c8cfbcaadb1d813425701e29020c95
Caleb Nance <calebnance@gmail.com>
2020-12-16 23:21:26 -0500

    upgraded to expo sdk 40

--- src/screens/More.js
+++ src/screens/More.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { Linking } from 'expo';
+import * as Linking from 'expo-linking';
 import { gStyle } from '../constants';
 
 // components
