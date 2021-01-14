20a04b59023c7416c4778084b51e7c70f257aea1
Caleb Nance <calebnance@gmail.com>
2021-01-13 20:58:56 -0500

    added preloading and some more helpful comments

---
+++ src/constants/preloadFonts.js
@@ -0,0 +1,8 @@
+// app fonts preload
+// /////////////////////////////////////////////////////////////////////////////
+
+// add custom font requires here
+// const someFontName = require('../assets/fonts/[FONT NAME].ttf');
+
+// then add them to the obj below & add them in the `constants/fonts.js`
+export default [{}];
