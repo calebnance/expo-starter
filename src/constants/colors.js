7126a096d66b0bbadcdca38aab9897b32d323f26
calebnance <calebnance@gmail.com>
2021-04-07 23:31:03 -0400

    updated icons

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -6,10 +6,10 @@ export default {
   black60: 'rgba(0, 0, 0, 0.6)',
   black80: 'rgba(0, 0, 0, 0.8)',
 
-  itemActive: '#fff',
+  itemActive: '#ffffff',
   itemInactive: '#3282b8',
 
   primary: '#0f4c75',
 
-  white: '#fff'
+  white: '#ffffff'
 };
