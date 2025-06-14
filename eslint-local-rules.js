732f3c00de019a7db20cb6cee546d85ffe7a3f89
Caleb Nance <calebnance@gmail.com>
2025-06-13 21:23:39 -0400

    added custom eslint rule

---
+++ eslint-local-rules.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'lowercase-comments': {
+    create(context) {
+      return {
+        Program() {
+          const sourceCode = context.getSourceCode();
+          sourceCode.getAllComments().forEach((comment) => {
+            const commentText = comment.value.trim();
+
+            if (commentText && commentText !== commentText.toLowerCase()) {
+              context.report({
+                loc: comment.loc,
+                message: 'Comments must be all lowercase.'
+              });
+            }
+          });
+        }
+      };
+    }
+  }
+};
